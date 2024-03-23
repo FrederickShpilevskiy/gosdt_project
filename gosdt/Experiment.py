46717ae249c6be4d91d2f5959d5831286afc7dca
Ryan Shar <ryan.shar01@gmail.com>
2024-03-22 23:24:29 -0700

    Refactored GOSDT to use new implementation

--- gosdt/Experiment.py
+++ gosdt/Experiment.py
@@ -4,12 +4,19 @@ import pathlib
 
 from sklearn.ensemble import GradientBoostingClassifier
 from sklearn.tree import DecisionTreeClassifier
-from model.threshold_guess import compute_thresholds, cut
-from gosdt.model.gosdt import GOSDT
+# from model.threshold_guess import compute_thresholds, cut
+# from gosdt.model.gosdt import GOSDT
+from gosdt import ThresholdGuessBinarizer, GOSDTClassifier
 from DataSampler import generate_data
 
 # hyperparams
-TREE_DEPTH = None
+GBDT_N_EST = 40
+GBDT_MAX_DEPTH = 1
+REGULARIZATION = 0.001
+SIMILAR_SUPPORT = False
+DEPTH_BUDGET = None
+TIME_LIMIT = 60
+VERBOSE = False 
 
 # y should be numpy 
 # len(weights) == len(X) == len(y)
@@ -18,94 +25,48 @@ def weighted_loss(model, X, y, weights):
     loss = (y.reshape(-1) != preds).astype(float)
     return (loss * weights).sum()
 
-def preprocess_dataset(dataset, n_est=40, max_depth=1):
-    X, y = dataset.iloc[:,:-1].values, dataset.iloc[:,-1].values
-
-    # guess thresholds
-    X = pd.DataFrame(X, columns=dataset.columns[:-1])
-    X_train, thresholds, header, threshold_guess_time = compute_thresholds(X, y, n_est, max_depth)
-    y_train = pd.DataFrame(y)
-
-    # guess lower bound
-    clf = GradientBoostingClassifier(n_estimators=n_est, max_depth=max_depth, random_state=42)
-    clf.fit(X_train, y_train.values.flatten())
-    warm_labels = clf.predict(X_train)
-
-    # save the labels as a tmp file and return the path to it.
-    labelsdir = pathlib.Path('/tmp/warm_lb_labels')
-    labelsdir.mkdir(exist_ok=True, parents=True)
-
-    labelpath = labelsdir / 'warm_label.tmp'
-    labelpath = str(labelpath)
-    pd.DataFrame(warm_labels, columns=["class_labels"]).to_csv(labelpath, header="class_labels",index=None)
-
-    # train GOSDT model
-    config = {
-                "regularization": 0.001,
-                "depth_budget": TREE_DEPTH,
-                "time_limit": 60,
-                "warm_LB": True,
-                "path_to_labels": labelpath,
-                "similar_support": False,
-            }
-    return GOSDT(config), X_train, y_train, thresholds, header
-
-def apply_thresholds(dataset, thresholds, header):
-    X, y = dataset.iloc[:,:-1].values, dataset.iloc[:,-1].values
-    X = pd.DataFrame(X, columns=dataset.columns[:-1])
-    # apply thresholds
-    X_new = cut(X, thresholds)
-    return X_new[list(header)], pd.DataFrame(y)
-
-
-def run_gosdt(args, df, weights):
-    sampled_df = generate_data(args, df, weights)
-    model, X, y, thresholds, header = preprocess_dataset(sampled_df)
-
+def gosdt_experiment(args, df, weights, should_dup=False):
     if args.logs:
-        print(df.shape)
-        print(X.shape)
+        print("Gosdt Experiment")
 
-    # faulthandler.enable()
-
-    if args.logs:
-        print("right before gosdt fit")
-    model.fit(X, y)
-    if args.logs:
-        print("right after gosdt fit")
-     # not real "test" set, we are just interested in performance on all data
-     # need to change the data to have the right features for classifying
-    X_test, y_test = apply_thresholds(df, thresholds, header)
-    return weighted_loss(model, X_test, y_test.to_numpy(), weights)
-
-def run_scikit(args, df, weights):
-    sampled_df = generate_data(args, df, weights)
-    X, y = sampled_df.iloc[:,:-1].values, sampled_df.iloc[:,-1].values
-
-    clf = DecisionTreeClassifier(max_depth=TREE_DEPTH, random_state=42)
-    clf.fit(X, y)
-
-    # not real "test" set, we are just interested in performance on all data
-    X_test = df.iloc[:,:-1].values
-    y_test = df.iloc[:,-1].values
-    return weighted_loss(clf, X_test, y_test, weights)
+    if should_dup:
+        sampled_df = generate_data(args, df, weights)
+    else:
+        sampled_df = df.copy()
     
+    X, y = sampled_df.iloc[:, :-1], sampled_df.iloc[:, -1]
 
-def run_gosdt_fit_without_weights(args, df, weights):
-    model, X, y, thresholds, header = preprocess_dataset(df)
+    if args.logs:
+        print(f"X shape: {X.shape}")
+        print(f"y shape: {y.shape}")
 
-    model.fit(X, y)
+    threshold_enc = ThresholdGuessBinarizer(n_estimators=GBDT_N_EST, max_depth=GBDT_MAX_DEPTH, random_state=args.seed)
+    threshold_enc.set_output(transform="pandas")
+    X_guessed = threshold_enc.fit_transform(X, y)
 
-     # not real "test" set, we are just interested in performance on all data
-     # need to change the data to have the right features for classifying
-    X_test, y_test = apply_thresholds(df, thresholds, header)
-    wrong = model.predict(X_test) != y_test.to_numpy().reshape(-1)
-    return weighted_loss(model, X_test, y_test.to_numpy(), weights), wrong
+    clf = GOSDTClassifier(regularization=REGULARIZATION, similar_support=SIMILAR_SUPPORT, time_limit=TIME_LIMIT, depth_budget=DEPTH_BUDGET, verbose=VERBOSE) 
+    clf.fit(X_guessed, y)
+    
+    wrong = clf.predict(X_guessed) != y.to_numpy().reshape(-1) 
+
+    # Not a real test set, but need to apply thresholds to original data
+    X_test, y_test = df.iloc[:, :-1], df.iloc[:, -1]
+    X_test_enc = threshold_enc.transform(X_test)
+
+    return weighted_loss(clf, X_test_enc, y_test.to_numpy(), weights), wrong 
 
 
-def run_scikit_fit_without_weights(args, df, weights):
-    X, y = df.iloc[:,:-1].values, df.iloc[:,-1].values
-    clf = DecisionTreeClassifier(max_depth=TREE_DEPTH, random_state=42)
+def scikit_experiment(args, df, weights, should_dup=False):
+    if args.logs:
+        print("Scikit Experiment")
+
+    if should_dup:
+        sampled_df = generate_data(args, df, weights)
+    else:
+        sampled_df = df.copy()
+
+    X, y = sampled_df.iloc[:,:-1].values, sampled_df.iloc[:,-1].values
+    clf = DecisionTreeClassifier(max_depth=DEPTH_BUDGET, random_state=args.seed)
     clf.fit(X,y)
 
     # not real "test" set, we are just interested in performance on all data
@@ -127,42 +88,50 @@ def save_results(args, loss_arg, override_experiment=None):
             if add_header:
                 file.write('seed,sampling_method,data_gen,distribution,p,experiment,exp_params,tree_depth,loss\n')
             file.write(f'{args.seed}, {args.data_dup}, {data_source}, {args.weight_dist}({"-".join(map(str, args.weight_args))}), {args.p}, {args.experiment if override_experiment is None else override_experiment}, {args.exp_params}, {args.tree_depth}, {loss_arg}\n')
+    
+    else:
+        print(f"loss: {loss_arg}")
 
 def run_experiment(args, df, weights):
-    global TREE_DEPTH
-    TREE_DEPTH = args.tree_depth
+    global DEPTH_BUDGET
+    DEPTH_BUDGET = args.tree_depth
 
     # Set up the print stuff and 
 
     experiment = args.experiment
     if experiment == "gosdt":
-        loss = run_gosdt(args, df, weights)
+        loss, _ = gosdt_experiment(args, df, weights, should_dup=True)
         save_results(loss)
     elif experiment == "gosdt-fit-without-weights":
-        loss, _ = run_gosdt_fit_without_weights(args, df, weights)
-        save_results(loss)
+        loss, _ = gosdt_experiment(args, df, weights)
+        save_results(args, loss)
     elif experiment == "scikit":
-        loss = run_scikit(args, df, weights)
-        save_results(loss)
+        loss, _ = scikit_experiment(args, df, weights, should_dup=True)
+        save_results(args, loss)
     elif experiment == "scikit-fit-without-weights":
-        loss = run_scikit_fit_without_weights(args, df, weights)
-        save_results(loss)
+        loss, _ = scikit_experiment(args, df, weights)
+        save_results(args, loss)
     elif experiment == 'gosdt-bias-to-errors':
        # weights don't matter, create weights based on errors in unweighted tree
        if args.logs:
            print("Performing fit without weights")
-       init_loss, wrong = run_gosdt_fit_without_weights(args, df, weights)
 
+       init_loss, wrong = gosdt_experiment(args, df, weights)
        N = df.shape[0]
+
+       if args.logs:
+           print(f"Got {np.sum(wrong)} wrong with {N} points")
+
        bias_value = args.exp_params[0]
        bias_weights = np.ones(N)
        bias_weights[wrong] = bias_value
        bias_weights = bias_weights / np.sum(bias_weights)
        if args.logs:
            print("Made weights")
+           print(bias_weights[:10])
 
-       unweighted_tree_loss, _ = run_gosdt_fit_without_weights(args, df, bias_weights)
-       weighted_tree_loss = run_gosdt(args, df, bias_weights)
+       weighted_tree_loss, _ = gosdt_experiment(args, df, bias_weights, should_dup=True)
+       unweighted_tree_loss, _ = gosdt_experiment(args, df, bias_weights)
        if args.logs:
            print("done weighted tree")
        save_results(args, unweighted_tree_loss, override_experiment="unweighted_tree")
@@ -173,7 +142,7 @@ def run_experiment(args, df, weights):
 
     elif experiment == 'scikit-bias-to-errors':
         # weights don't matter, create weights based on errors in unweighted tree 
-        _, wrong = run_scikit_fit_without_weights(args, df, weights)
+        _, wrong = scikit_experiment(args, df, weights)
         
         N = df.shape[0]
         bias_value = args.exp_params[0]
@@ -181,8 +150,8 @@ def run_experiment(args, df, weights):
         bias_weights[wrong] = bias_value
         bias_weights = bias_weights / np.sum(bias_weights)
 
-        unweighted_tree_loss, _ = run_scikit_fit_without_weights(args, df, bias_weights)
-        weighted_tree_loss = run_scikit(args, df, bias_weights)
+        weighted_tree_loss, _ = scikit_experiment(args, df, bias_weights, should_dup=True)
+        unweighted_tree_loss, _ = scikit_experiment(args, df, bias_weights)
         save_results(args, unweighted_tree_loss, override_experiment="unweighted_tree")
         save_results(args, weighted_tree_loss, override_experiment="weighted_tree")
 
